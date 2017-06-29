debccecb8ebc60a8d351b2c2a734b63764d3c000
Alan Aberdeen <alanaberdeen@gmail.com>
2017-06-29 16:20:26 +0100

    Linter complaints

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import App from './App'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App';
 
-Vue.use(Vuetify)
-Vue.config.productionTip = false
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
 
 // Event bus to handle child-child communication
-export const eventBus = new Vue(); 
+export const eventBus = new Vue();
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
