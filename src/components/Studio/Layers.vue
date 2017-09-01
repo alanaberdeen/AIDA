5453bd2b8b4c53b0e6ef286fbffe0bd0ef8d0c8d
Alan Aberdeen <alanaberdeen@gmail.com>
2017-09-01 16:56:03 +0100

    Revert "Revert "Revert "Revert "Layer naming clash""""
    This reverts commit f70eef56c5464195156ca9fd0745d357ea00d84c.

--- src/components/Studio/Layers.vue
+++ src/components/Studio/Layers.vue
@@ -69,7 +69,12 @@ export default {
     },
 
     created() {
-        this.paperScope.project.activeLayer.name = 'Layer 1'
+
+        // If there are no layers imported, then the default layer will not
+        // have a name. So best set it here and avoid confusion.
+        if (!this.paperScope.project.activeLayer.name){
+            this.paperScope.project.activeLayer.name = 'Layer 1'
+        }
     },
 
     methods: {
