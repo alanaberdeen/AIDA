51bdd8393f5926fe2c02d2ec1a371fca2364e4cc
Alan Aberdeen <alanaberdeen@gmail.com>
2017-09-14 21:04:06 +0100

    Vue-router install
    Thought about using Vue-router to manage the different pages on the
    site but realised we are going to have an issue because the Vue
    instance still isn’t aware of the canvas on which paperJS and
    Openseadragon work.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+
+// Routing
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// CSS material design framework.
 import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
+// Application child component
 import App from './App';
 
-Vue.use(Vuetify);
+// Development
 Vue.config.productionTip = false;
 
 // Event bus to handle child-child communication
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6 */
+
+export const routes = [
+    { path: '/'}
+];
