d6e5826c6e5757014cb949a38e889bf039b8da08
Alan Aberdeen <alanaberdeen@gmail.com>
2017-09-28 12:21:12 +0100

    Tool tooltips

--- src/components/Editor/Tools/Tools.vue
+++ src/components/Editor/Tools/Tools.vue
@@ -5,48 +5,56 @@
 
             <app-pan :active="(this.activeTool === 'pan')"
                      @click.native="activeTool = 'pan'"
-                     v-if="(this.getConfigStepTools().includes('pan'))">
+                     v-if="(this.getConfigStepTools().includes('pan'))"
+                     v-tooltip:right="{ html: this.tools.pan.caption }">
             </app-pan>
 
             <v-divider></v-divider>
 
             <app-move :active="(this.activeTool === 'move')"
                       @click.native="activeTool = 'move'"
-                      v-if="(this.getConfigStepTools().includes('move'))">
+                      v-if="(this.getConfigStepTools().includes('move'))"
+                      v-tooltip:right="{ html: this.tools.move.caption }">
             </app-move>
 
             <app-node :active="(this.activeTool === 'node')"
                       @click.native="activeTool = 'node'"
-                      v-if="(this.getConfigStepTools().includes('node'))">
+                      v-if="(this.getConfigStepTools().includes('node'))"
+                      v-tooltip:right="{ html: this.tools.node.caption }">
             </app-node>
 
             <v-divider></v-divider>
 
             <app-circle :active="(this.activeTool === 'circle')"
                         @click.native="activeTool = 'circle'"
-                        v-if="(this.getConfigStepTools().includes('circle'))">
+                        v-if="(this.getConfigStepTools().includes('circle'))"
+                        v-tooltip:right="{ html: this.tools.circle.caption }">
             </app-circle>
 
             <app-rectangle  :active="(this.activeTool === 'rectangle')"
                             @click.native="activeTool = 'rectangle'"
-                            v-if="(this.getConfigStepTools().includes('circle'))">
+                            v-if="(this.getConfigStepTools().includes('rectangle'))"
+                            v-tooltip:right="{ html: this.tools.rectangle.caption }">
             </app-rectangle>
 
             <app-pen  :active="(this.activeTool === 'pen')"
                       @click.native="activeTool = 'pen'"
-                      v-if="(this.getConfigStepTools().includes('circle'))">
+                      v-if="(this.getConfigStepTools().includes('pen'))"
+                      v-tooltip:right="{ html: this.tools.pen.caption }">
             </app-pen>
 
             <app-pencil :active="(this.activeTool === 'pencil')"
                         @click.native="activeTool = 'pencil'"
-                        v-if="(this.getConfigStepTools().includes('circle'))">
+                        v-if="(this.getConfigStepTools().includes('pencil'))"
+                        v-tooltip:right="{ html: this.tools.pencil.caption }">
             </app-pencil>
 
             <v-divider></v-divider>
 
             <app-count :active="(this.activeTool === 'count')"
                        @click.native="activeTool = 'count'"
-                       v-if="(this.getConfigStepTools().includes('count'))">
+                       v-if="(this.getConfigStepTools().includes('count'))"
+                       v-tooltip:right="{ html: this.tools.count.caption }">
             </app-count>
 
             <v-divider></v-divider>
@@ -80,7 +88,8 @@ export default {
 
     computed: {
         ...mapState({
-            activeStep: state => state.config.activeStep
+            activeStep: state => state.config.activeStep,
+            tools: state => state.config.tools
         })
     },
 
@@ -103,6 +112,10 @@ export default {
 }
 </script>
 
+<!-- //TODO: Manage tooltip delay in but no delay out. Also if tooltip already
+             displayed then shouldn't have further delay on moving to a different
+             tool.  -->
+
 <style lang="css" scoped>
 
 #toolsPanel {
@@ -114,4 +127,7 @@ export default {
     background: #EEEEEE;
 }
 
+[data-tooltip]:before {
+    transition-delay: 800ms;
+}
 </style>
