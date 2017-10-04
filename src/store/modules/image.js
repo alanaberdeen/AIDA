93fa0d47554189b2028eea813fb7444a75307162
Alan Aberdeen <alanaberdeen@gmail.com>
2017-10-04 15:18:21 +0100

    Dependency fixes

--- src/store/modules/image.js
+++ src/store/modules/image.js
@@ -117,10 +117,7 @@ const mutations = {
 	// Add images to the openseadragon Viewer
 	addImages: (state, channels) => {
 
-
 		for (let image in channels){
-
-            console.log(channels[image].url)
 
             // If pulling from secure data scource then need to set the correct
             // params: ajaxWithCredentials, loadTilesWithAjax, ajaxHeaders.
