dab013346e2f9b243714574aa701628574f49e3f
Alan Aberdeen <alanaberdeen@gmail.com>
2018-05-26 14:35:49 +0100

    Add routes for DZI and simple example
    Roots also pass props to the components being loaded.

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,38 @@ import Editor from '@/components/editor/Editor'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Landing',
-      component: Landing
-    },
-    {
-      path: '/example',
-      name: 'Example',
-      component: Editor
+  routes: [{
+    path: '/',
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/example/dzi',
+    name: 'DZI Example',
+    component: Editor,
+    props: {
+      default: true,
+      type: 'dzi'
     }
+  },
+  {
+    path: '/example/simple',
+    name: 'Simple Example',
+    component: Editor,
+    props: {
+      default: true,
+      type: 'simple'
+    }
+  },
+  {
+    path: '/example',
+    name: 'Examples',
+    component: Editor,
+    props: {
+      default: true,
+      type: 'examples'
+    }
+  }
+
   ]
 })
