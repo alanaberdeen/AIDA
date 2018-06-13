c6c0493d5fbb759f2f80ca5a8861065d9223913e
Alan Aberdeen <alanaberdeen@gmail.com>
2018-06-13 16:32:05 +0100

    Move dev and build tooling to Vue CLI

--- src/main.js
+++ src/main.js
@@ -1,75 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions,
-  Vcard,
-  VMenu,
-  VDivider,
-  VStepper,
-  VSlider,
-  VDialog,
-  VTabs,
-  VTextField,
-  VTooltip,
-  VSubheader,
-  VRadioGroup,
-  VForm
-} from 'vuetify'
-import '../node_modules/vuetify/src/stylus/app.styl'
-import './css/main.css'
+// Vuetify content
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
-    VBtn,
-    VIcon,
-    VGrid,
-    VToolbar,
-    transitions,
-    Vcard,
-    VMenu,
-    VDivider,
-    VStepper,
-    VSlider,
-    VDialog,
-    VTabs,
-    VTextField,
-    VTooltip,
-    VSubheader,
-    VRadioGroup,
-    VForm
-  }
-})
+import './css/main.css' // Ensure you are using css-loader
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 // Event bus to handle child-child communication
 export const eventBus = new Vue()
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  components: {
-    App
-  },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
