b387f6d357dd37672cc939754429f3a10c1241ed
Alan Aberdeen <alanaberdeen@gmail.com>
2018-06-27 12:33:38 +0100

    BUG: Fix a get default colors issue

--- src/store/modules/annotation/getters.js
+++ src/store/modules/annotation/getters.js
@@ -3,7 +3,7 @@ export default {
   // Get default color for the current layer
   getDefaultLayerColor: (state, getters, rootState) => {
     let activeLayerIndex = rootState.editor.activeLayer
-    let numberLayers = state.project.layers.length
-    return state.defaultColors[activeLayerIndex % numberLayers]
+    let numberColors = state.defaultColors.length
+    return state.defaultColors[activeLayerIndex % numberColors]
   }
 }
