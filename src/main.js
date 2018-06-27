e71e88a97674eb184af3bda479d4456acbb9bf63
Alan Aberdeen <alanaberdeen@gmail.com>
2018-06-27 11:33:09 +0100

    BUG: pass alternate config to veeValidate to ensure prop names don't clash

--- src/main.js
+++ src/main.js
@@ -16,13 +16,15 @@ import VeeValidate from 'vee-validate'
 VeeValidate.Validator.extend('verify_password', {
   getMessage: field => 'The password must contain at least: 1 uppercase letter, 1 lowercase letter, 1 number, and one special character (E.g. , . _ & ? etc)',
   validate: value => {
-    var strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})')
+    var strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})')
     return strongRegex.test(value)
   }
 })
 
 Vue.use(Vuetify)
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+  fieldsBagName: 'vee-fields'
+})
 
 Vue.config.productionTip = false
 
