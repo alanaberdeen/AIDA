fc4241c76fde64d182cf608b366bd198db71252c
Alan Aberdeen <alanaberdeen@gmail.com>
2018-06-28 14:13:10 +0100

    Move default colors out of getter and into state

--- src/store/modules/annotation/getters.js
+++ src/store/modules/annotation/getters.js
@@ -8,8 +8,7 @@ export default {
       // If there is no specified step color then return the default layer color
     } else {
       let activeLayerIndex = rootState.editor.activeLayer
-      let defaultColors = ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd']
-      return defaultColors[activeLayerIndex % defaultColors.length]
+      return state.defaultColors[activeLayerIndex % state.defaultColors.length]
     }
   }
 }
