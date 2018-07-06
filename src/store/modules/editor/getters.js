2589962c9106344da931e02bcbe402bba226a676
Alan Aberdeen <alanaberdeen@gmail.com>
2018-07-06 16:55:26 +0100

    Only return step tools when they are specifically set

--- src/store/modules/editor/getters.js
+++ src/store/modules/editor/getters.js
@@ -1,7 +1,7 @@
 export default {
   // Get an array specifying the tools included in the current step.
   getStepTools: state => {
-    if (state.steps) {
+    if (state.steps && state.steps.tools) {
       return state.steps[state.activeStep].tools
     } else {
       return ['pan', 'circle', 'rectangle', 'pen', 'pencil', 'move', 'node', 'count', 'delete', 'faces']
