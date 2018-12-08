a57ad591e6c25a501c984db543116ada39ec57ea
Alan <alanaberdeen@gmail.com>
2018-12-08 14:55:37 +0000

    Add polygon tool to default set

--- src/store/modules/editor/getters.js
+++ src/store/modules/editor/getters.js
@@ -4,7 +4,7 @@ export default {
     if (state.steps && state.steps[state.activeStep].tools) {
       return state.steps[state.activeStep].tools
     } else {
-      return ['pan', 'circle', 'rectangle', 'path', 'pencil', 'move', 'node', 'count', 'delete', 'megas', 'filter']
+      return ['pan', 'circle', 'rectangle', 'path', 'pencil', 'polygon', 'move', 'node', 'count', 'delete', 'megas', 'filter']
     }
   }
 }
