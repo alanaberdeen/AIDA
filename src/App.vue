f855fb913e5414d2a291bff6579d4dd494063039
Alan <alanaberdeen@gmail.com>
2019-06-16 12:58:14 +0100

    Add cookie snackbar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,43 @@
     <v-content>
       <router-view/>
     </v-content>
+    <v-snackbar
+      v-model="snackbar"
+      :timeout=0
+    >
+      This site uses cookies to function optimally.
+      <v-btn
+        dark
+        flat
+        @click="acceptCookies()"
+      >
+        OK
+      </v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
+  data () {
+    return {
+      snackbar: true
+    }
+  },
+
+  methods:  {
+    acceptCookies() {
+      this.snackbar = false
+      window.localStorage.setItem('acceptCookies', 'false')
+    }
+  },
+
+  created () {
+    if (window.localStorage.acceptCookies && window.localStorage.acceptCookies === 'false') {
+      this.snackbar = false
+    }
+  }
 }
 </script>
