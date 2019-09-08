fe3ed0236d9393f330847739a77064a3487e1652
Alan <alanaberdeen@gmail.com>
2019-09-08 14:21:33 +0100

    Add vue config

---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // If deploying to GH pages then need to include this as the public path for the correct repository. 
+    publicPath: process.env.NODE_ENV === 'production' ? '/AIDA/' : '/'
+}
