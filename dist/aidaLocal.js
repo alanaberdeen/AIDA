9233f11af7fbb984b7ba4cd019eca015f6e2abe0
Alan Aberdeen <alanaberdeen@gmail.com>
2020-04-07 16:05:48 +0100

    new build

--- dist/aidaLocal.js
+++ dist/aidaLocal.js
@@ -1,57 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const history = require('connect-history-api-fallback')
 const fsp = require('fs').promises
 const path = require('path')
 const chalk = require('chalk')
 const ip = require('ip')
-const fs = require('fs');
-const ini = require('ini');
+const fs = require('fs')
+const ini = require('ini')
 
 // Check for images in the data/images/
 // Write a file an array of the available images as reference.
 
-const config = ini.parse(fs.readFileSync(path.join(__dirname, 'config.ini'), 'utf-8'));
-const imagesDir = path.isAbsolute(config.images_dir) ? config.images_dir : path.join(__dirname, config.images_dir);
-const annotationsDir = path.isAbsolute(config.annotations_dir) ? config.annotations_dir : path.join(__dirname, config.annotations_dir);
-const iiifHostname = config['IIIF'].hostname.toString();
-const iiifPort = parseInt(config['IIIF'].port.toString(), 10);
-const iiifHttps = (config['IIIF'].https.toString().toLowerCase() == 'true');
+const config = ini.parse(fs.readFileSync(path.join(__dirname, 'config.ini'), 'utf-8'))
+const dataDir = path.isAbsolute(config.data_dir) ? config.data_dir : path.join(__dirname, config.data_dir)
+const annotationsDir = path.isAbsolute(config.annotations_dir) ? config.annotations_dir : path.join(__dirname, config.annotations_dir)
+const iiifHostname = config.IIIF.hostname.toString()
+const iiifPort = parseInt(config.IIIF.port.toString(), 10)
+const iiifHttps = (config.IIIF.https.toString().toLowerCase() === 'true')
 
-async function checkForImages () {
-  const images = await walk(imagesDir, imagesDir)
-  const json = JSON.stringify(images)
-  await fsp.writeFile(path.join(__dirname,'data', 'images.json'), json, 'utf8')
-}
-
-// Recursively build the filetree by searching through all directories
-// Do not search inside DZI file directories (too many irrelevant files)
-// ignore .DS_store files
-async function walk (dir, rootDir, fileList = []) {
+async function walk (dir, rootDir) {
+  const fileList = []
   const files = await fsp.readdir(dir)
-  for (const file of files) {
-    const stat = await fsp.stat(path.join(dir, file))
-
-    if (stat.isDirectory() && !file.endsWith('_files')) {
-      fileList.push({
-        name: path.basename(file),
-        children: []
-      })
-
-      const children = fileList[fileList.length - 1].children
-      await walk(path.join(dir, file), rootDir, children)
-    } else if (file !== '.DS_Store' && !file.endsWith('_files')) {
-      fileList.push({
-        name: file,
-        ext: path.extname(file),
-        path: path.relative(rootDir, path.join(dir, file))
-      })
+  for (const fileName of files) {
+    const filePath = path.join(dir, fileName)
+    const fileInfo = {
+      name: fileName,
+      path: path.relative(rootDir, filePath)
     }
+    const fileStat = await fsp.stat(filePath)
+    if (fileStat.isDirectory()) {
+      fileInfo.children = []
+    } else if (fileStat.isFile()) {
+      fileInfo.ext = path.extname(fileName)
+    }
+    fileList.push(fileInfo)
   }
   return fileList
 }
 
-async function saveAnnotation (data) {
+async function saveAnnotation (payload) {
   // Get the IP address of the current machine. If we need to write a new raster
   // file we should save the link to it using the IP address. This ensures the
   // API endpoint where the image can be found is referenced correctly even
@@ -60,7 +46,7 @@ async function saveAnnotation (data) {
 
   // Check for raster image items and if found we need to save these as image
   // files and replace the item.source with link to that file.
-  data.annotation.layers.forEach(layer => {
+  payload.annotation.data.layers.forEach(layer => {
     layer.items.forEach((item, index) => {
       if (item.type === 'raster' && item.source.substring(0, 4) === 'data') {
         const imagePath = path.join(annotationsDir, 'raster', index + '_' + layer.name + '.png')
@@ -75,8 +61,9 @@ async function saveAnnotation (data) {
   })
 
   // Write annotation data as JSON file.
-  const annotationFilePath = path.join(annotationsDir, data.images[0].name + '.json')
-  const json = JSON.stringify(data.annotation)
+  let annotationFilePath = payload.annotation.filePath ? payload.annotation.filePath : path.join(annotationsDir, payload.images[0].name + '.json')
+  annotationFilePath = `${annotationsDir}/${annotationFilePath}`
+  const json = JSON.stringify(payload.annotation.data)
 
   try {
     await fsp.writeFile(annotationFilePath, json, 'utf8')
@@ -109,15 +96,13 @@ async function startServer () {
     next()
   })
 
-  // Use history proxy middleware to handle 404 routes in SPA
-  app.use(history())
-
   // Need to parse POST Body data (for parsing application/json)
   // Increase the limit from the default (100kb) to enable large annotation JSON
   // files
   app.use(bodyParser.json({
     limit: '100mb'
   }))
+  app.use(bodyParser.text())
 
   // Save annoation data
   app.post('/save', async function (req, res) {
@@ -132,10 +117,11 @@ async function startServer () {
   })
 
   // Check for images that may have been added to the directory
-  app.post('/checkForImages', async function (req, res) {
+  app.post('/checkForImagesAndProjects', async function (req, res) {
     try {
-      await checkForImages()
-      res.send('Success, found images')
+      const childDir = req.body
+      const items = await walk(path.join(dataDir, childDir), dataDir)
+      res.send(items)
     } catch (err) {
       console.log('Could not check for images')
       console.log(err)
@@ -143,19 +129,16 @@ async function startServer () {
     }
   })
 
-  // Serve image files
-  app.use('/images', express.static(imagesDir, {}))
-
-  // Serve static annotation data
+  // Serve static  data
   // Always respond with headers that disable the cache. Specifically this is
   // important when editing raster images. Without this, when the editor is
   // reloaded the browser will serve the original, pre-edit, from the cache.
-  app.use('/annotations', express.static(annotationsDir, {
+  app.use('/data', express.static(dataDir, {
     setHeaders: function (res, path) {
       res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
     }
   }))
-  
+
   app.get('/iiif', function (req, res) {
     protocol = iiifHttps ? 'https' : 'http'
     res.send(new URL('/iiif/2/', protocol + '://' + iiifHostname + ':' + iiifPort).toString())
@@ -164,15 +147,20 @@ async function startServer () {
   // Serve the built application
   app.use(express.static(__dirname))
 
+  app.route('/*')
+    .get(function (req, res) {
+      res.sendFile(path.join(__dirname, 'index.html'))
+    })
+
   // Get the IP address of the current machine. The application will also be
   // usable over the network from this address.
   const networkIPAddress = ip.address()
 
   // Listen to requests
   app.listen(port, () => {
-    console.log(`  AIDA running at:`)
+    console.log('  AIDA running at:')
     console.log('  - Local:   ' + chalk.cyan(`http://localhost:${port}/dashboard`))
-    console.log('  - Network: ' + chalk.cyan(`http://` + networkIPAddress + `:${port}/dashboard`))
+    console.log('  - Network: ' + chalk.cyan('http://' + networkIPAddress + `:${port}/dashboard`))
   })
 }
 
