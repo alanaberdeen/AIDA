e4060f15ab6acaf429ab1e5949ffc6c1fea99ab2
Alan Aberdeen <alanaberdeen@gmail.com>
2021-09-13 15:47:45 +0100

    Add overview

--- src/components/Viewer2D/Settings/Overview.tsx
+++ src/components/Viewer2D/Settings/Overview.tsx
@@ -1,38 +1,69 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from "react";
 
-import Map from 'ol/Map'
-import OverviewMap from 'ol/control/OverviewMap'
-import TileLayer from 'ol/layer/Tile'
-import Zoomify from 'ol/source/Zoomify'
+import Map from "ol/Map";
+import OverviewMap from "ol/control/OverviewMap";
+import TileLayer from "ol/layer/Tile";
+import Zoomify from "ol/source/Zoomify";
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 // Overview shows a high-level overview of the map, indicating the current size
 // and position of the viewport.
 const Overview = (props: { map: Map }) => {
-  const { map } = props
+  const { map } = props;
 
-  const overviewEl = useRef(null)
+  const overviewEl = useRef(null);
+
+  const [isOpen, setIsOpen] = useState(true)
 
   useEffect(() => {
-    const imageLayer = map.getLayers().getArray()
-      .find(layer => layer.get('id') === 'image') as TileLayer<Zoomify>
-    const imageSource = imageLayer.getSource()
+    const imageLayer = map
+      .getLayers()
+      .getArray()
+      .find((layer) => layer.get("id") === "image") as TileLayer<Zoomify>;
+    const imageSource = imageLayer.getSource();
 
     const overview = new OverviewMap({
-      className: 'overview ol-overviewmap',
+      className: "overview ol-overviewmap",
       collapsible: false,
-      layers: [new TileLayer({
-        source: imageSource,
-      })],
+      layers: [
+        new TileLayer({
+          source: imageSource,
+        }),
+      ],
       target: overviewEl.current,
-    })
-    map.addControl(overview)
-  }, [map])
-
+    });
+    map.addControl(overview);
+  }, [map]);
 
   return (
-    <div ref={overviewEl} className="w-full h-32 bg-white"/>
-  )
-}
+    <div>
+      <button
+        className={classNames(
+          isOpen
+            ? "rounded-t-md border-b-2"
+            : "rounded-md border-b-0",
+          "text-gray-700 hover:bg-gray-50 hover:text-gray-900 bg-white group w-full flex items-center pr-2 py-2 text-left text-sm font-medium focus:outline-none focus:ring-2 focus:ring-blue-500 relative z-10"
+        )}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <svg
+          className={classNames(
+            isOpen ? "text-gray-400 rotate-90" : "text-gray-300",
+            "mr-2 flex-shrink-0 h-5 w-5 transform group-hover:text-gray-400 transition-colors ease-in-out duration-150"
+          )}
+          viewBox="0 0 20 20"
+          aria-hidden="true"
+        >
+          <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
+        </svg>
+        Overview
+      </button>
+      <div ref={overviewEl} className={`${!isOpen ? 'hidden' : ''} w-full h-32 bg-white rounded-b-md`}/>
+    </div>
+  );
+};
 
-export default Overview
+export default Overview;
