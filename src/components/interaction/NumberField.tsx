6b1f9e85ba40d65e16bb09e47d80753e8a4fb0cd
Alan Aberdeen <alanaberdeen@gmail.com>
2021-09-13 15:47:30 +0100

    Interaction components

---
+++ src/components/interaction/NumberField.tsx
@@ -0,0 +1,81 @@
+// Adapted from:
+// https://react-spectrum.adobe.com/react-aria/useNumberField.html
+
+import {useNumberFieldState} from '@react-stately/numberfield';
+import {useLocale} from '@react-aria/i18n';
+import {useButton} from '@react-aria/button';
+import {useNumberField} from '@react-aria/numberfield';
+
+import {useRef} from 'react';
+
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid'
+
+const NumberField = (props) => {
+  let {locale} = useLocale();
+  let state = useNumberFieldState({...props, locale});
+  let inputRef = useRef();
+  const incrementRef = useRef();
+  const decrementRef = useRef();
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') state.commit();
+  }
+
+  let {
+    labelProps,
+    groupProps,
+    inputProps,
+    incrementButtonProps,
+    decrementButtonProps
+  } = useNumberField({...props, onKeyDown: handleKeyDown}, state, inputRef);
+
+  let {buttonProps: incrementProps} = useButton(incrementButtonProps, incrementRef);
+  let {buttonProps: decrementProps} = useButton(decrementButtonProps, decrementRef);
+
+  return (
+    <div>
+      {
+        props.label &&
+        <label
+          className="block text-sm font-medium text-gray-700"
+          {...labelProps}
+          htmlFor={inputRef.current}
+        >
+          {props.label}
+        </label>
+      }
+      <div
+        {...groupProps}
+        className={`${props.label ? 'mt-1' : ''} relative rounded-md shadow-sm flex`}
+      >
+        <input
+          type="number"
+          {...inputProps}
+          ref={inputRef}
+          id={inputRef.current}
+          className="pl-1 focus:ring-blue-500 w-full focus:border-blue-500 min-width-0 text-sm border border-gray-300 rounded-l-md"
+        />
+
+        <div className="flex flex-col border border-gray-300 rounded-r-md">
+          <button
+            className="-ml-px relative inline-flex items-center px-1 text-sm font-medium rounded-r-md rounded-b-none text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+            {...incrementProps}
+            ref={incrementRef}
+          >
+            <ChevronUpIcon className="h-3 w-3" aria-hidden="true" />
+          </button>
+          <button
+            className="-ml-px relative inline-flex items-center px-1 text-sm font-medium rounded-r-md rounded-t-none text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+            {...decrementProps}
+            ref={decrementRef}
+          >
+            <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default NumberField;
---
+++ src/components/interaction/Slider.tsx
@@ -0,0 +1,97 @@
+// Adapted from https://react-spectrum.adobe.com/react-aria/useSlider.html
+
+import {useSliderState} from '@react-stately/slider';
+import {useFocusRing} from '@react-aria/focus';
+import {VisuallyHidden} from '@react-aria/visually-hidden';
+import {mergeProps} from '@react-aria/utils';
+import {useNumberFormatter} from '@react-aria/i18n';
+import { useSlider, useSliderThumb } from '@react-aria/slider';
+
+import { useRef } from 'react'
+
+import NumberField from './NumberField';
+
+const Slider = (props) => {
+  let trackRef = useRef(null);
+  let numberFormatter = useNumberFormatter(props.formatOptions);
+  let state = useSliderState({...props, numberFormatter});
+  let {groupProps, trackProps} = useSlider(
+    props,
+    state,
+    trackRef
+  );
+
+  return (
+    <div
+      {...groupProps}
+      className="w-full p-2"
+      style={{ touchAction: 'none' }}>
+
+      {/* Create a flex container for the label and output element. */}
+      {/* <div className="flex self-stretch justify-between text-sm text-gray-700">
+        {props.label && <label {...labelProps}>{props.label}</label>}
+        <output {...outputProps}>
+          {state.getThumbValueLabel(0)}
+        </output>
+      </div> */}
+
+      {/* The track element holds the visible track line and the thumb. */}
+      <div className="w-full pl-2 flex items-center">
+        <div
+          {...trackProps}
+          ref={trackRef}
+          className="relative h-4 flex-1"
+        >
+          <div
+            className="absolute bg-gray-500 top-2 w-full -translate-y-1/2 flex-1"
+            style={{ height: 2 }}
+          />
+          <Thumb index={0} state={state} trackRef={trackRef} />
+        </div>
+        
+        <div className="ml-3 text-sm w-16 text-gray-700 flex-shrink">
+          <NumberField {...props} />
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+const Thumb = (props) => {
+  let {state, trackRef, index} = props;
+  let inputRef = useRef(null);
+  let {thumbProps, inputProps} = useSliderThumb(
+    {
+      index,
+      trackRef,
+      inputRef
+    },
+    state
+  );
+
+  let {focusProps, isFocusVisible} = useFocusRing();
+  return (
+    <div
+      className="absolute top-2 -translate-x-1/2 -translate-y-1/2"
+      style={{ left: `${state.getThumbPercent(index) * 100}%` }}>
+      <div
+        {...thumbProps}
+        className="w-3 h-3 rounded-full"
+        style={{
+          backgroundColor: isFocusVisible
+            ? '#1E40AF' // blue-800
+            : state.isThumbDragging(index)
+            ? '#374151' // gray-700
+            : '#6B7280' // gray-500
+        }}>
+        <VisuallyHidden>
+          <input ref={inputRef} {...mergeProps(inputProps, focusProps)} />
+        </VisuallyHidden>
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
+
