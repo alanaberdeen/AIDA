9e7d4f9593a7aa4cb68945ec28d5d0d50e53a536
Alan Aberdeen <alanaberdeen@gmail.com>
2021-09-14 12:47:51 +0100

    Edit layer names

--- src/components/Viewer2D/Settings/Layers/Layer.tsx
+++ src/components/Viewer2D/Settings/Layers/Layer.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, SyntheticEvent } from 'react'
 
 import Map from "ol/Map";
 import VectorLayer from "ol/layer/Vector";
@@ -16,6 +16,16 @@ const Layer = (props: {
   const { layer, active, map } = props;
 
   const [isVisible, setIsVisible] = useState(layer.getVisible());
+
+  // Utilities for editing layer name
+  const [isEditingName, setIsEditingName] = useState(false);
+  const nameInputRef = useRef(null);
+  const [layerName, setLayerName] = useState(layer.get('id'))
+  const handleNameChange = (e: SyntheticEvent) => {
+    const newName = (e.target as HTMLInputElement).value;
+    setLayerName(newName);
+    layer.set('id', newName);
+  }
 
   // Toggle opacity
   const opacityCache = useRef(layer.getOpacity());
@@ -55,13 +65,37 @@ const Layer = (props: {
         }
       </button>
 
+      {/*
+        This feels like a bit of a hack. We need to display the text input
+        on doubleClick. But we also want to automatically focus the input. 
+        Since you cannot apply focus to an unmounted/hidden DOM element it's not
+        easy to toggle between them. Settled on a hack where we instead change 
+        the element widths. Likely there is a cleaner solution.
+      */}
+      <input
+        type="text"
+        name="layer-name"
+        ref={nameInputRef}
+        className={`${isEditingName ? 'w-full m-1' : 'w-0'} block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 focus:border-blue-500 border-gray-300`}
+        placeholder={layerName}
+        onBlur={() => { setIsEditingName(false) }}
+        value={layerName}
+        onChange={handleNameChange}
+        onKeyUp={(e) => {if (e.key === 'Enter') { nameInputRef.current.blur() }}}
+      />
+      
       <button
         type="button"
-        className={`${active ? 'text-blue-900 bg-gray-50 font-medium' : 'text-gray-700 bg-white' } m-1 truncate inline-flex items-center w-full hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+        className={`${active ? 'text-blue-900 bg-gray-50 font-medium' : 'text-gray-700 bg-white' } ${isEditingName ? 'w-0' : 'w-full m-1 '} inline-flex truncate items-center w-full hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
         onClick={() => setLayerActive(layer)}
+        onDoubleClick={() => {
+          setIsEditingName(true)
+          nameInputRef.current.focus()
+        }}
       >
-        {layer.get('id')}
-      </button>
+        {layerName}
+      </button> 
+      
     </div>
   );
 };
