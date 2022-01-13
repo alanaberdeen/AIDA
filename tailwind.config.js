bd5f4ddbc3e2944272ef40c5c9f0dab25e4a1c12
Alan Aberdeen <alanaberdeen@gmail.com>
2022-01-13 12:00:00 +0000

    Update tailwind config for new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,4 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        teal: colors.teal
-      }
-    }
-  },
   plugins: []
 }
